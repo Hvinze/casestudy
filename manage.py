e6c0598a5922142973fe9d19ab66fba359f41378
Harshit Vinze <hvinze.hv@gmail.com>
2021-12-24 16:28:23 +0530

    Changes made in branch2

--- manage.py
+++ manage.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quora.settings")
 
     from django.core.management import execute_from_command_line
+    #Made changes in branch2
 
     execute_from_command_line(sys.argv)
